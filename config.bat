7a905433e5da4ff2867fb7cf28d6af833799aceb
Gavin <gmy.gavin@gmail.com>
2015-03-31 19:47:03 +0800

    config.bat

---
+++ config.bat
@@ -0,0 +1,8 @@
+@javac -d .\bin .\src\*.java
+@echo Compiling Process Completed.
+@echo.
+@echo Please run the program using command like: 
+@echo.
+@echo "java -classpath bin Performer [tc\tai75a.dat]"
+@echo.
+cmd 
